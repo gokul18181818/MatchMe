6c65e3e0c706e290eb76c7eb92dcd44ee95dabc2
gokul18181818 <gokulpremkumar2003@gmail.com>
2025-06-08 08:46:08 +0530

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      srcDir: 'public',
+      filename: 'manifest.webmanifest',
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
